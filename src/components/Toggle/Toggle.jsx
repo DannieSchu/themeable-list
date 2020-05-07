c9098359adb1b2addc2821080f75e6aab6b1c760
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-06 19:23:50 -0700

    create toggle component

---
+++ src/components/Toggle/Toggle.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './toggle.css';
+
+const Toggle = ({ toggle, value }) => {
+  return (
+    <section className={styles.Toggle}>
+      <input id="toggle" type="checkbox" checked={value} onChange={toggle} />
+      <label htmlFor="toggle" />
+    </section>
+  );
+};
+
+Toggle.propTypes = {
+  toggle: PropTypes.func.isRequired,
+  value: PropTypes.bool.isRequired,
+};
+
+export default Toggle;
+
