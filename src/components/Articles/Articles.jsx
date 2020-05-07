172b8b451620f491d3a9114e73f64a3272d11f5f
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-06 23:00:08 -0700

    add conditional styling

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Article from './Article.jsx';
-import { useArticles } from '../../hooks/NewsProvider.jsx';
+import { useArticles, useCountry } from '../../hooks/NewsProvider.jsx';
+import styles from './Articles.css';
 
 const Articles = () => {
   const articles = useArticles();
+  const country = useCountry();
 
   const articleEntries = articles.map((article, i) => (
     <li key={i}>
@@ -12,7 +14,7 @@ const Articles = () => {
   ));
 
   return (
-    <ul>
+    <ul className={`${styles.Articles} ${styles[country]}`}>
       {articleEntries}
     </ul>
   );
