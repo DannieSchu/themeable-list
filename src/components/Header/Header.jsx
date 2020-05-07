1d2e017528381a1aa9600446b3394f8adfc45762
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-07 00:43:55 -0700

    import paging into header and footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import styles from './Header.css';
 import Toggle from '../Toggle/Toggle';
-import { useToggle, useCountry } from '../../hooks/NewsProvider';
+import { useToggle, useCountry, usePaging, usePageNumber } from '../../hooks/NewsProvider';
+import Paging from '../Paging/Paging';
 
 const Header = () => {
   const toggle = useToggle();
   const country = useCountry();
+  const incrementPage = usePaging();
+  const page = usePageNumber();
 
   return (
     <header className={styles.Header}>
-      <h1>Cross-Pond Newsroom</h1>
+      <section>
+        <h1>Cross-Pond Newsroom</h1>
+        <Paging incrementPage={incrementPage} page={page}/>
+      </section>
       <section>
         <span>UK</span>
         <Toggle toggle={toggle} value={country === 'us'} />
