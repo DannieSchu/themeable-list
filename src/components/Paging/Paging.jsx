5011ade0349f47f5a7f71cf5d3b14ac6329e5212
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-07 00:43:04 -0700

    style paging component

--- src/components/Paging/Paging.jsx
+++ src/components/Paging/Paging.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Paging.css'
 
 const Paging = ({ incrementPage, page }) => {
   return (
-    <div>
+    <article className={styles.Paging}>
       <button onClick={() => incrementPage(-1)} disabled={page === 1}>&lt;</button>
       <button onClick={() => incrementPage(1)} disabled={page === 4}>&gt;</button>
-    </div>
+    </article>
   );
 };
 
